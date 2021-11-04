cf3a764e29e04f4793f5408229f35d4fb07f34c8
Azise <aziseit.pro@gmail.com>
2021-11-04 07:22:04 -0400

    Ajout Obstacle

---
+++ Assets/Scripts/Sol.cs
@@ -0,0 +1,36 @@
+
+using UnityEngine;
+
+public class Sol : MonoBehaviour
+{
+    AspetSol aspetSol;
+
+    private void Start()
+    {
+        aspetSol = GameObject.FindObjectOfType<AspetSol>();
+        SpawnObstacle();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        aspetSol.PointSol();
+        Destroy(gameObject, 2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject obstaclePrefab;
+    void SpawnObstacle()
+    {
+        //choisir un point au hasar pour afficher l'obstacle'
+        int obstacleSpawnIndex = Random.Range(2, 5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+        //afficher l'obstacle a la position'
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+}
